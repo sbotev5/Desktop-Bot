98459672002c1ca61da930341566d1b3a7f80cb1
sbotev5 <stoyanbotev5@gmail.com>
2017-07-17 23:45:47 +0300

    Add files via upload

--- KeyListener.java
+++ KeyListener.java
@@ -7,18 +7,16 @@ public class KeyListener implements NativeKeyListener {
     public void nativeKeyPressed(NativeKeyEvent e) {
 
         if (Menu.shouldRecord) {
+
             Menu.userMovements.put("KeyBoard" + Menu.keyPressCounter, e.getKeyCode());
             Menu.keyPressCounter++;
+
         }
 
-
-        //System.out.println("KEY PRESSED");
-
-        //hi
     }
 
     public void nativeKeyReleased(NativeKeyEvent e) {
-       // System.out.println("KEY RELEASED");
+
     }
 
     public void nativeKeyTyped(NativeKeyEvent e) {
