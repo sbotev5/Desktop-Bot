630b4dc3fa56aa35333456800212b1611e8285f2
sbotev5 <stoyanbotev5@gmail.com>
2017-07-27 23:13:30 +0300

    Save recordings on files functionality

---
+++ Movement.java
@@ -0,0 +1,19 @@
+import java.io.Serializable;
+
+public class Movement implements Serializable{
+    private String type;
+    private Object movement;
+
+    Movement(String type, Object movement) {
+        this.type = type;
+        this.movement = movement;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Object getMovement() {
+        return movement;
+    }
+}
